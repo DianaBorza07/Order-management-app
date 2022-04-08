5e7b26baf3d7266c1c399ac16b6d12d1dc4d2907
DianaBorza07 <94463656+DianaBorza07@users.noreply.github.com>
2022-04-08 16:32:22 +0300

    Update ConnectionFactory.java

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -18,7 +18,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DBURL = "jdbc:mysql://localhost:3306/warehouse";
     private static final String USER = "root";
-    private static final String PASS = "Diana07#";
+    private static final String PASS = "";
 
     private static ConnectionFactory singleInstance = new ConnectionFactory();
 
